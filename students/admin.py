7df78e031bddca51f42c61668da118a4347eb35f
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2021-04-22 21:30:15 +0100

    Remove students application code

--- students/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from students import models
-
-admin.site.register(models.Student)
--- students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Student(models.Model):
-    name = models.TextField()
-    average_grade = models.FloatField()
-
-    def __str__(self):
-        return self.name
